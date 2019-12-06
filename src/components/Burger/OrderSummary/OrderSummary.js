d684ea85294e7b17179467c10f3e4e0489b529b4
Ken <kensong96@gmail.com>
2019-12-06 16:29:43 -0500

    167D

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Auxi from "../../../hoc/Auxi";
+
+const OrderSummary = props => {
+    const ingredientSummary = Object.keys(props.ingredients).map(igKey => {
+        return (
+            <li key={igKey}>
+                <span style={{textTransform: "capitalize"}}>{igKey}</span>:
+                {props.ingredients[igKey]}
+            </li>
+        );
+    });
+    return (
+        <Auxi>
+            <h3>Your order</h3>
+            <p>A delicious burger with the following ingredients:</p>
+            <ul>{ingredientSummary}</ul>
+            <p>Continue to check out</p>
+        </Auxi>
+    );
+};
+
+export default OrderSummary;
