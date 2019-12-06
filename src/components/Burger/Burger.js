f3d92e21dc6c4de2447227cdb1320c0225feeb0d
Ken <kensong96@gmail.com>
2019-12-05 23:45:22 -0500

    159D

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,16 @@ import classes from "./Burger.css";
 import BurgerIngredient from "./BurgerIngredient/Burgeringredient";
 
 const burger = props => {
+    const transfomedIngredients = Object.keys(props.ingredients).map(igKey => {
+        return [...Array(props.ingredients[igKey])].map((_, i) => {
+            return <BurgerIngredient key={igKey + i} type={igKey} />;
+        });
+    });
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"></BurgerIngredient>
-            <BurgerIngredient type="cheese"></BurgerIngredient>
-            <BurgerIngredient type="meat"></BurgerIngredient>
+            {transfomedIngredients}
             <BurgerIngredient type="bread-bottom"></BurgerIngredient>
         </div>
     );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,10 +3,23 @@ import Auxi from "./../../hoc/Auxi";
 import Burger from "../../components/Burger/Burger";
 
 class BurgerBuilder extends Component {
+    // constructor(props) {
+    //     super(props);
+    //     this.state={...};
+
+    state = {
+        ingredients: {
+            salad: 1,
+            bacon: 1,
+            cheese: 2,
+            meat: 2
+        }
+    };
+
     render() {
         return (
             <Auxi>
-                <Burger></Burger>
+                <Burger ingredients={this.state.ingredients} />
                 <div>Burger controls</div>
             </Auxi>
         );
